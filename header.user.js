bca43fc5da955596c869289c79fc5e10eaa012b4
got2bhockey <7141971+got2bhockey@users.noreply.github.com>
2026-06-23 17:02:48 -0400

    Added header + adjusted workflow

---
+++ header.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name Battlemetrics Toolkit - Desktop
+// @namespace https://www.battlemetrics.com/
+// @version 12.19
+// @updateURL https://raw.githubusercontent.com/liquid-gaming/bm-userscript/main/bm-toolkit-desktop.user.js
+// @downloadURL https://raw.githubusercontent.com/liquid-gaming/bm-userscript/main/bm-toolkit-desktop.user.js
+// @description Modifies the rcon panel for battlemetrics to help color code important events and details about players.
+// @author Got2bhockey
+// @match https://www.battlemetrics.com/*
+// @icon https://www.google.com/s2/favicons?sz=64&domain=battlemetrics.com
+// @grant GM_addStyle
+// @grant GM_xmlhttpRequest
+// @connect communitybanlist.com
+// @run-at document-end
+// ==/UserScript==
